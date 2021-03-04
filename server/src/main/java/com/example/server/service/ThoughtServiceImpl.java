ce1a07add2e95099262ea2d9a451f24d31bd5d6c
Avery Jones <averyjonesphoto@mac.com>
2021-03-04 16:45:07 -0500

    finished service implementation

--- server/src/main/java/com/example/server/service/ThoughtServiceImpl.java
+++ server/src/main/java/com/example/server/service/ThoughtServiceImpl.java
@@ -1,14 +1,28 @@
 package com.example.server.service;
 
+import com.example.server.model.Thought;
 import com.example.server.repository.ThoughtRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ThoughtServiceImpl {
+public class ThoughtServiceImpl implements ThoughtService{
     @Autowired
     ThoughtRepository thoughtRepository;
 
     @Override
+    public Iterable<Thought> getThoughts() { return thoughtRepository.findAll(); }
 
+    @Override
+    public Thought createThought(Thought thought) { return thoughtRepository.save(thought); }
+
+    @Override
+    public Thought updateThought(Thought thought) { return thoughtRepository.save(thought); }
+
+    @Override
+    public HttpStatus deleteThought(Long id){
+        thoughtRepository.deleteById(id);
+        return HttpStatus.OK;
+    }
 }
